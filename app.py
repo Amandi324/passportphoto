51bcc22ac9236c9669c9840f5e43985a852c8491
Amandi324 <amandisandi324@gmail.com>
2026-03-16 23:04:12 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, send_file
+from rembg import remove
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/remove-bg', methods=['POST'])
+def remove_bg():
+
+    if 'image' not in request.files:
+        return {"error": "No image uploaded"}
+
+    file = request.files['image']
+
+    input_image = Image.open(file.stream)
+
+    output = remove(input_image)
+
+    img_io = io.BytesIO()
+    output.save(img_io, 'PNG')
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
