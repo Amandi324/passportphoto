ff51d0421066653e447f3f4127c2dcb31ed2b881
Amandi324 <amandisandi324@gmail.com>
2026-03-17 08:46:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,30 +6,29 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-
-@app.route('/remove-bg', methods=['POST'])
+@app.route("/remove-bg", methods=["POST"])
 def remove_bg():
 
-    if 'image' not in request.files:
+    if "image" not in request.files:
         return {"error": "No image uploaded"}
 
-    file = request.files['image']
+    file = request.files["image"]
 
     input_image = Image.open(file.stream)
 
     output = remove(input_image)
 
     img_io = io.BytesIO()
-    output.save(img_io, 'PNG')
+    output.save(img_io, "PNG")
     img_io.seek(0)
 
-    return send_file(img_io, mimetype='image/png')
+    return send_file(img_io, mimetype="image/png")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
